2ee82eea9b4d9ee506f68a58dbe6042f623afddd
Jose Naime <espinin123@gmail.com>
2018-11-24 11:09:25 -0600

    adaw

---
+++ Simulador-Tecbot/Assets/Points.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+using UnityEngine;
+using TMPro;
+
+public class Points : MonoBehaviour {
+    public int points;
+    private TextMeshPro score;
+
+    void Awake()
+    {
+    }
+    public void AddPoints(int poinsToAdd)
+    {
+        points += poinsToAdd;
+       
+        
+    }
+}
---
+++ Simulador-Tecbot/Assets/PointsTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointsTrigger : MonoBehaviour {
+    public int objetos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Object"){
+            objetos++;
+            Points.FindObjectOfType<Points>().AddPoints(1);
+        }
+    }
+    private void OnTriggerStay(Collider col)
+    {
+        print(col.gameObject);
+       if(objetos >= 4)
+        {
+            
+            Destroy(col.gameObject);
+            Points.FindObjectOfType<Points>().AddPoints(20);
+        }
+    }
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Object")
+        {
+            objetos--;
+        }
+    }
+}
